9c82dc698319e2de6c34255de3d58f1e9a0845cc
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-07-30 04:47:10 +0500

    applied some temporary styles for container

--- src/components/Container/container.tsx
+++ src/components/Container/container.tsx
@@ -1,7 +1,10 @@
 import { FunctionComponent } from "react";
+import cs from "classnames";
+import classes from "./container.module.scss";
+import classNames from "classnames";
 
 const Container: FunctionComponent = ({ children }) => {
-  return <div className="container">{children}</div>;
+  return <div className={classNames(classes.container)}>{children}</div>;
 };
 
 export default Container;
