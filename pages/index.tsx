b56378a9644ebd9654d8e888127e9a914cd281b3
Zieu <zieu2000@gmail.com>
2021-07-29 22:28:14 +0500

    set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Qoraqalam</div>;
+  return (
+    <div>
+      <h1>Qoraqalam</h1>
+      <Link href="/components">
+        <a>Components</a>
+      </Link>
+    </div>
+  );
 }
