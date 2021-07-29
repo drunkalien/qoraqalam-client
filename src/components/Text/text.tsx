70c132cdb32ec4379341a8c2a1caf8b3697bf515
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-07-30 04:50:51 +0500

    applied styles

--- src/components/Text/text.tsx
+++ src/components/Text/text.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from "react";
 import cn from "classnames";
-import classes from "./Text.module.scss";
+import classes from "./text.module.scss";
 
 type TextProps = {
-  size: "xs" | "sm" | "md" | "lg" | "xl";
-  color: "white" | "black";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  color?: "white" | "black";
   children: ReactNode;
 };
 
 const Text = ({ size = "sm", color = "black", children }: TextProps) => {
-  return <span className={cn(`text`, classes.size, classes.color)}>{children}</span>;
+  return (
+    <span className={cn(classes.text, classes[size], classes[color])}>
+      {children}
+    </span>
+  );
 };
 
 export default Text;
