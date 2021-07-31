f5008152648fd5b187bd4594d748817fdd66e7f4
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-07-31 19:20:27 +0500

    created header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,23 @@
+import cn from "classnames";
+import classes from "./header.module.scss";
+import Container from "../Container/container";
+import Searchbar from "../SearchbarComponent";
+import Button from "../Button";
+import { Logo } from "../../assets/icons";
+
+const Header = () => {
+  return (
+    <Container>
+      <div className={cn(classes.logoContainer)}>
+        <Logo />
+        qoraqalam<span className={cn(classes.dot)}>.</span>
+      </div>
+      <Searchbar />
+      <Button color="black" variant="filled">
+        Maqola yozish
+      </Button>
+    </Container>
+  );
+};
+
+export default Header;
