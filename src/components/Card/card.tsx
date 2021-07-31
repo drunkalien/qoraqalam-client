ea4d281229e397a01920297a3ce6a281fc702188
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-07-31 19:20:05 +0500

    created card component

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
+import cn from "classnames";
+import classes from "./card.module.scss";
 
 type CardProps = {
   children: ReactNode;
 };
 
 const Card = ({ children }: CardProps) => {
-  return <div>{children}</div>;
+  return <div className={cn(classes.card)}>{children}</div>;
 };
 
 export default Card;
