2cc085502f66f09da03e8e36dbd9f5097a2c2c7f
Ziyodulla Muydinjanov <zieu2000@gmail.com>
2021-08-02 16:04:50 +0500

    added paragraphs

--- components/Pages/Post/post.tsx
+++ components/Pages/Post/post.tsx
@@ -1,4 +1,5 @@
 import { Page, Heading, Hashtag } from "components";
+import Text from "components/Text";
 import classes from "./post.module.scss";
 
 const Post = () => {
@@ -15,6 +16,20 @@ const Post = () => {
             {tag}
           </Hashtag>
         ))}
+      </div>
+      <div className={classes["paragraph-container"]}>
+        <Text>
+          Milliy gvardiya mashinalari derazalar ostidan o’tib, bizdan uyda
+          qolishimizni so’rayotgan vaqtda, internet ham karantinga qanday rioya
+          qilish haqida turli xil maslahatlar, ogohlantirishlar va hikoyalarga
+          to’la.
+        </Text>
+        <Text>
+          Biroq, COVID-19 sochlar orqali yuqishi haqidagi xabarlar kabi ko’plab
+          hikoyalar yolg’on. Biz siz uchun O’zbekistondagi karantin qoidalari
+          haqida bilishingiz kerak bo’lgan barcha ma’lumotni bir joyga
+          to’pladik.
+        </Text>
       </div>
     </Page>
   );
