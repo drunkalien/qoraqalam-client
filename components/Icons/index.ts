c6eddc27c135230049e1e64f703c925fe290bf18
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-08-03 21:21:42 +0500

    added icons

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,3 +1,8 @@
 export { default as Star } from "./Star";
 export { default as Calendar } from "./Calendar";
 export { default as Eye } from "./Eye";
+export { default as Telegram } from "./Telegram";
+export { default as Instagram } from "./Instagram";
+export { default as Phone } from "./Phone";
+export { default as Facebook } from "./Facebook";
+export { default as LogoWhite } from "./LogoWhite";
