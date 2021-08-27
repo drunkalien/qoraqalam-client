8e00a737ffc5bfd5d6abde56401f9d5df5baf779
Zieu <zieu2000@gmail.com>
2021-08-28 00:00:52 +0500

    translated and added placeholder

--- components/Pages/Login/_signup-form/_sign-up.form.tsx
+++ components/Pages/Login/_signup-form/_sign-up.form.tsx
@@ -6,9 +6,17 @@ const Form = () => {
   return (
     <div className={cn(classes["form-container"])}>
       <form className={cn(classes.form)}>
-        <h1 className={cn(classes["form-heading"])}>Login</h1>
-        <Input label="Email or Username" type="email" />
-        <Input label="Password" type="password" />
+        <h1 className={cn(classes["form-heading"])}>Kirish</h1>
+        <Input
+          label="Email yoki username"
+          type="email"
+          placeholder="bu@misol.uz"
+        />
+        <Input
+          label="Parol"
+          placeholder="Parolingizni kiriting"
+          type="password"
+        />
         <Button isLarge color="blue" type="button" className="mt-5">
           Davom etish
         </Button>
