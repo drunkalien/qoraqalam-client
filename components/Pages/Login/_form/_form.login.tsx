4cba3535f732c0fd5c83ff55f4940e78bbd75f5b
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-08-31 01:30:15 +0500

    login form validation

--- components/Pages/Login/_form/_form.login.tsx
+++ components/Pages/Login/_form/_form.login.tsx
@@ -1,24 +1,63 @@
 import cn from "classnames";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import classes from "../_login.module.scss";
 import { Button, Input } from "components";
 import Link from "next/link";
 
+type Data = {
+  emailOrUsernam: string;
+  password: string;
+};
+
+const schema = yup.object().shape({
+  emailOrUsername: yup.string().required("Email yoki Usernameni kiriting!"),
+  password: yup.string().required("Parolni kiriting!"),
+});
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function submit(data: Data) {
+    console.log(data);
+  }
+
   return (
     <div className={cn(classes["form-container"])}>
-      <form className={cn(classes.form)}>
+      <form className={cn(classes.form)} onSubmit={handleSubmit(submit)}>
         <h1 className={cn(classes["form-heading"])}>Kirish</h1>
         <Input
           label="Email yoki username"
           type="email"
           placeholder="bu@misol.uz"
+          style={errors.emailOrUsername ? { borderColor: "red" } : undefined}
+          {...register("emailOrUsername")}
         />
+        {errors.emailOrUsername ? (
+          <p className={cn(classes["error-msg"])}>
+            {errors.emailOrUsername?.message}
+          </p>
+        ) : null}
+
         <Input
           label="Parol"
           placeholder="Parolingizni kiriting"
           type="password"
+          style={errors.password ? { borderColor: "red" } : undefined}
+          {...register("password")}
         />
-        <Button isLarge color="blue" type="button" className="mt-5">
+        {errors.password ? (
+          <p className={cn(classes["error-msg"])}>{errors.password?.message}</p>
+        ) : null}
+
+        <Button isLarge color="blue" type="submit" className="mt-5">
           Davom etish
         </Button>
         <p className="text-center mt-10 fz-14">
