d10d4e44de5931e002652950220a17d115202097
Zieu <zieu2000@gmail.com>
2021-09-05 21:28:26 +0500

    next-env type for base url added

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+declare module NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
+  }
+}
