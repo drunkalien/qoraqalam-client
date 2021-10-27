8727ff982be505d32f9056368c79ce2121321193
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-10-27 14:56:39 +0500

    added send request to create article mutation

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import cn from "classnames";
+import { useForm } from "react-hook-form";
+import { useAPIMutation } from "hooks";
+import toast from "react-hot-toast";
 
 import { Button, Heading } from "components";
 
@@ -10,34 +13,49 @@ const Editor = () => {
   const [article, setArticle] = useState<string>("");
   const [heading, setHeading] = useState("");
   const [preview, setPreview] = useState(false);
+  const { register, handleSubmit } = useForm();
+
+  const CreateArticleMutation = useAPIMutation({ url: "articles" });
+
+  const onSubmit = handleSubmit((data) => {
+    const mutationPromise = CreateArticleMutation.mutateAsync(data);
+
+    toast
+      .promise(mutationPromise, {
+        loading: "Kuting...",
+        success: "Muvaffaqiyatli",
+        error: "Muvaffaqiyatsiz",
+      })
+      .then(() => {})
+      .catch(() => {});
+  });
 
   return !preview ? (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className={cn(classes["editor-container"])}>
         <textarea
-          name="heading"
           id="heading"
           placeholder="Sarlavha"
           className={cn(classes["heading-input"])}
+          {...register("title")}
           onChange={(e) => setHeading(e.target.value)}
           value={heading}
         ></textarea>
         <textarea
-          name="article"
           id="article"
           placeholder="Maqola Yozing"
           className={cn(classes.editor)}
+          {...register("content")}
           onChange={(e) => setArticle(e.target.value)}
           value={article}
         ></textarea>
       </div>
       <div className={cn(classes.buttons)}>
-        <Button>{"Jo'natish"}</Button>
+        <Button type="submit">{"Jo'natish"}</Button>
         <Button type="button" color="blue" onClick={() => setPreview(true)}>
           {"Prevyu"}
         </Button>
       </div>
-      {/* @ts-ignore */}
     </form>
   ) : (
     <div className={cn(classes.preview)}>
